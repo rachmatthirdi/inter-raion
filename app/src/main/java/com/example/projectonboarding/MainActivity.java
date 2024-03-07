28a0d85762efed12399cfa948eaf3ece3affb343
rachmatthirdi <rachmatthirdi@gmail.com>
2024-03-07 15:15:01 +0700

    buat navbar

--- app/src/main/java/com/example/projectonboarding/MainActivity.java
+++ app/src/main/java/com/example/projectonboarding/MainActivity.java
@@ -1,24 +1,54 @@
 package com.example.projectonboarding;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+
+    BeritaFragment beritaFragment= new BeritaFragment();
+    BerandaFragment berandaFragment= new BerandaFragment();
+    CariFragment cariFragment= new CariFragment();
+    ProfileFragment profileFragment= new ProfileFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BerandaFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                if (item.getItemId() == R.id.nav_beranda) {
+                    selectedFragment = new BerandaFragment();
+                } else if (item.getItemId() == R.id.nav_cari) {
+                    selectedFragment = new CariFragment();
+                } else if (item.getItemId() == R.id.nav_berita) {
+                    selectedFragment = new BeritaFragment();
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
         });
+
     }
 }
